aa92f5b23d51eaee1e1beafff25f11a27df197ac
harvey <harveytseng2@gmail.com>
2024-08-24 13:45:52 -0400

    adds starting deepgram

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 import { Bot, LifeBuoy, Mic, SquareUser, User, Disc } from "lucide-react";
-
+import {
+  LiveConnectionState,
+  LiveTranscriptionEvent,
+  LiveTranscriptionEvents,
+  useDeepgram,
+} from "../context/DeepgramContextProvider";
+import {
+  MicrophoneEvents,
+  MicrophoneState,
+  useMicrophone,
+} from "../context/MicrophoneContextProvider";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,12 +19,13 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Message {
   type: "user" | "bot";
   content: string;
 }
+
 
 export default function Dashboard() {
   const [isRecording, setIsRecording] = useState(false);
@@ -25,41 +36,105 @@ export default function Dashboard() {
     },
   ]);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const audioContextRef = useRef<AudioContext | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const handleMicClick = async () => {
-    if (isRecording) {
-      // Stop recording logic here
-      if (mediaRecorderRef.current) {
-        mediaRecorderRef.current.stop();
-        mediaRecorderRef.current.stream.getTracks().forEach((track) => track.stop());
-        mediaRecorderRef.current = null;
+  const [caption, setCaption] = useState<string | undefined>(
+    "Powered by Deepgram"
+  );
+  const { connection, connectToDeepgram, connectionState } = useDeepgram();
+  const { setupMicrophone, microphone, startMicrophone, microphoneState } =
+    useMicrophone();
+  const captionTimeout = useRef<any>();
+  const keepAliveInterval = useRef<any>();
+
+  useEffect(() => {
+    setupMicrophone();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (microphoneState === MicrophoneState.Ready) {
+      connectToDeepgram({
+        model: "nova-2",
+        interim_results: true,
+        smart_format: true,
+        filler_words: true,
+        utterance_end_ms: 3000,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [microphoneState]);
+
+  useEffect(() => {
+    if (!microphone) return;
+    if (!connection) return;
+
+    const onData = (e: BlobEvent) => {
+      // iOS SAFARI FIX:
+      // Prevent packetZero from being sent. If sent at size 0, the connection will close. 
+      if (e.data.size > 0) {
+        connection?.send(e.data);
       }
-      setIsRecording(false);
-      console.log("Recording stopped");
-      return;
+    };
+
+    const onTranscript = (data: LiveTranscriptionEvent) => {
+      const { is_final: isFinal, speech_final: speechFinal } = data;
+      let thisCaption = data.channel.alternatives[0].transcript;
+
+      console.log("thisCaption", thisCaption);
+      if (thisCaption !== "") {
+        console.log('thisCaption !== ""', thisCaption);
+        setCaption(thisCaption);
+      }
+
+      if (isFinal && speechFinal) {
+        clearTimeout(captionTimeout.current);
+        captionTimeout.current = setTimeout(() => {
+          setCaption(undefined);
+          clearTimeout(captionTimeout.current);
+        }, 3000);
+      }
+    };
+
+    if (connectionState === LiveConnectionState.OPEN) {
+      connection.addListener(LiveTranscriptionEvents.Transcript, onTranscript);
+      microphone.addEventListener(MicrophoneEvents.DataAvailable, onData);
+
+      startMicrophone();
     }
 
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const mediaRecorder = new window.MediaRecorder(stream);
-      mediaRecorderRef.current = mediaRecorder;
+    return () => {
+      // prettier-ignore
+      connection.removeListener(LiveTranscriptionEvents.Transcript, onTranscript);
+      microphone.removeEventListener(MicrophoneEvents.DataAvailable, onData);
+      clearTimeout(captionTimeout.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connectionState]);
 
-      mediaRecorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          // Handle the recorded data (e.g., save it or send it to a server)
-          console.log("Data available: ", event.data);
-        }
-      };
+  useEffect(() => {
+    if (!connection) return;
 
-      mediaRecorder.start();
-      setIsRecording(true);
-      console.log("Recording started", stream);
-    } catch (err) {
-      console.error("Error accessing microphone", err);
-      setError("Error accessing microphone");
+    if (
+      microphoneState !== MicrophoneState.Open &&
+      connectionState === LiveConnectionState.OPEN
+    ) {
+      connection.keepAlive();
+
+      keepAliveInterval.current = setInterval(() => {
+        connection.keepAlive();
+      }, 10000);
+    } else {
+      clearInterval(keepAliveInterval.current);
     }
-  };
+
+    return () => {
+      clearInterval(keepAliveInterval.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [microphoneState, connectionState]);
+
 
   return (
     <div className="grid h-screen w-full">
@@ -144,7 +219,6 @@ export default function Dashboard() {
                       variant="ghost"
                       size="icon"
                       className="animate-pulse border-2 border-gray-300 rounded-full p-2 size-16"
-                      onClick={handleMicClick}
                     >
                       {isRecording ? (
                         <Disc className="size-16" />
